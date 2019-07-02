8809c46711ab302a5bcb7ca7f1bbe4c049d6ee7c
wanglady <1843827089@qq.com>
2019-07-02 20:35:41 +0800

    dialog

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,87 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
+  <div id="app"
+       class="textarealist">
+
     <router-view/>
   </div>
 </template>
 
 <script>
+import md5 from './../config/md5.js'
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      a: { 'type': 'runoob', 'value': 10000, 'name': 'runoob', 'age': 'www.runoob.com', 'zhang': 'runoob', 'params': 'www.runoob.com' },
+      c: { 'type': [{
+        beta: 'aini',
+        deactivated: 'wang',
+        app: '2332',
+        children: 'leng'
+      }],
+      'value': 10000,
+      'name': 'runoob',
+      'age': 'www.runoob.com',
+      'zhang': 'runoob',
+      'params': 'www.runoob.com' },
+
+      d: { 'type': [{
+        beta: 'aini',
+        deactivated: 'wang',
+        app: '2332',
+        children: 'leng'
+      }, {
+        eta: 'aini',
+        heactivated: 'wang',
+        gpp: '2332',
+        children: 'leng'
+      }, {
+        ieta: 'aini',
+        deactivated: 'wang',
+        gpp: '2332',
+        zhildren: 'leng'
+      }],
+      'value': 10000,
+      'name': 'runoob',
+      'age': 'www.runoob.com',
+      'zhang': 'runoob',
+      'params': 'www.runoob.com' }
+    }
+  },
+  mounted () {
+    // this.ismd5encrypt(this.a)
+    // this.ismd5encrypt(this.c)
+    this.ismd5encrypt(this.d)
+  },
+  methods: {
+    ismd5encrypt: function (params) {
+      // console.time('loadImg') 开始时间
+      console.log('params', params)
+      let keys = []
+      // 排序
+      for (let k in params) {
+        if (params.hasOwnProperty(k)) {
+          keys.push(k)
+        }
+      }
+      keys.sort()
+      console.log('leys', keys)
+      let newParams = {}
+      for (let i = 0; i < keys.length; i++) {
+        let k = keys[i]
+        newParams[k] = params[k]
+
+        if (typeof (params[k]) === 'object') {
+          this.ismd5encrypt(params[k])
+        }
+      }
+      console.log('排序后---', newParams)
+      newParams = md5(newParams.toString())
+      console.log('md5后', newParams)
+      // console.timeEnd('loadImg') 结束时间
+      return newParams
+    }
+  }
 }
 </script>
 
---
+++ src/components/dia.vue
@@ -0,0 +1,57 @@
+<template>
+  <el-dialog :visible.sync="dialogVisible"
+             width="30%"
+             :show-close='false'
+             :modal='false'
+             :close-on-press-escape='false'
+             :close-on-click-modal='false'>
+    <i v-if='iconType === "success"'
+       class="icon iconfont  iconDia icon-chenggong"></i>
+    <i v-if='iconType === "fail"'
+       class="icon iconfont  iconDia icon-warning-circle1"></i>
+    <img v-if='iconType === "loading"'
+         src="../../assets/img/loading.gif"
+         style="width:40px;height:40px">
+
+    <span>{{message}}</span>
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  name: 'dia',
+  data () {
+    return {
+      dialogVisible: false
+    }
+  },
+  props: {
+    showDialog: { // 是否打开弹窗
+      type: Boolean,
+      default: false
+    },
+    message: { // 提示信息
+      type: String,
+      default: ''
+    },
+    iconType: { // 成功失败的图标显示
+      type: String,
+      default: ''
+    }
+  },
+  created () {
+    this.dialogVisible = this.showDialog
+  },
+  watch: {
+    showDialog: { // 监控传过来的是否打开
+      handler (val) {
+        this.dialogVisible = val
+      }
+    },
+    deep: true
+  }
+}
+</script>
+
+<style>
+</style>
